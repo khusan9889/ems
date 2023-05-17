d1317cb59c617f7bc0c7ecc0da90e619de9d975d
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-05-17 18:55:13 +0500

    Relations for User table added

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,21 +3,24 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\Scopes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Scopes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+    public $fillable = [
         'name',
         'email',
         'password',
@@ -41,4 +44,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function acs(): HasMany
+    {
+        return $this->hasMany(ACS::class);
+    }
+
+    public function polytrauma(): HasMany
+    {
+        return $this->hasMany(Polytrauma::class);
+    }
 }
