10ec4002c0f1427aecbd098e4846851a6145818d
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-05-17 17:20:59 +0500

    relation in migrations fixed, new Branch model created

--- database/migrations/2023_05_17_111444_create_acs_table.php
+++ database/migrations/2023_05_17_111444_create_acs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('acs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('branch_id')->nullable()->constrained('branch')->nullOnDelete();
-            $table->foreignId('user_id')->nullable()->constrained('user')->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->string('department')->nullable();
             $table->string('history_disease')->nullable();
             $table->string('full_name')->nullable();
@@ -50,9 +50,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('acs');
--- database/migrations/2023_05_17_114937_create_polytrauma_table.php
+++ database/migrations/2023_05_17_114937_create_polytrauma_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('polytrauma', function (Blueprint $table) {
             $table->id();
             $table->foreignId('branch_id')->nullable()->constrained('branch')->nullOnDelete();
-            $table->foreignId('user_id')->nullable()->constrained('user')->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->string('department')->nullable();
             $table->string('history_disease')->nullable();
             $table->string('full_name')->nullable();
