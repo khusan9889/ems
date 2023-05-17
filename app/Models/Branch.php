00215999733e4e6897370ea09e46cfa9de2a52e7
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-05-17 17:26:17 +0500

    Branch model created

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\Scopes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Branch extends Model
 {
-    use HasFactory;
+    use HasFactory, Scopes;
+
+    public $fillable = [
+        'name',
+    ];
+
+    public $hidden = [
+        'created_at', 'updated_at'
+    ];
 }
