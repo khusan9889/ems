d9e9171f04b76e647a4e15ef792f6410c0f2a819
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-05-25 16:03:34 +0500

    basic filter forms added

--- resources/views/acs/index.blade.php
+++ resources/views/acs/index.blade.php
@@ -14,8 +14,6 @@
 
         <thead>
             <tr>
-                
-
                 <th>id</th>
                 <th>department</th>
                 <th>history_disease</th>
@@ -27,12 +25,42 @@
                 <th>stat_department_full_name</th>
                 <th>Actions</th> <!-- Empty header for the icon column -->
             </tr>
+            <tr>
+                <td>
+                    <select class="input-group input-group-sm mb-3 form-control" type="text" name="" id="">
+                </td>
+                <td>
+                    <select class="input-group input-group-sm mb-3 form-control" type="text" name="department">
+                </td>
+                <td>
+                    <input class="input-group input-group-sm mb-3 form-control" type="text" name="history_disease">
+                </td>
+                <td>
+                    <input class="input-group input-group-sm mb-3 form-control" type="text" name="full_name">
+                </td>
+                <td>
+                    <input class="input-group input-group-sm mb-3 form-control" type="text" name="hospitalization_date">
+                </td>
+                <td>
+                    <input class="input-group input-group-sm mb-3 form-control" type="text" name="discharge_date">
+                </td>
+                <td>
+                    <select class="input-group input-group-sm mb-3 form-control" type="text" name="hospitalization_channels">
+                </td>
+                <td>
+                    <select class="input-group input-group-sm mb-3 form-control" type="text" name="physician_full_name">
+                </td>
+                <td>
+                    <select class="input-group input-group-sm mb-3 form-control" type="text" name="stat_department_full_name">
+                </td>
+            </tr>
+
         </thead>
         <tbody>
             @foreach($data as $key => $item)
             <tr>
                 <td>{{$item->id}}</td>
-                <td>{{$item->department}}</td>image.png
+                <td>{{$item->department}}</td>
                 <td>{{$item->history_disease}}</td>
                 <td>{{$item->full_name}}</td>
                 <td>{{$item->hospitalization_date}}</td>
