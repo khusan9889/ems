cc9c0666ab19288fd4409701eb694d893e6722dc
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-05-30 11:27:46 +0500

    represent table as a separate card

--- resources/views/dashboard/pages/full-table-polyt.blade.php
+++ resources/views/dashboard/pages/full-table-polyt.blade.php
@@ -1,92 +1,145 @@
 @extends('dashboard.layouts.default')
 
-
 @section('content')
-<!-- begin breadcrumb -->
-<!-- <ol class="breadcrumb float-xl-right">
-	<li class="breadcrumb-item"><a href="javascript:;">Дашборд</a></li>
-	<li class="breadcrumb-item active">Дашборд</li>
-</ol> -->
-<!-- end breadcrumb -->
-<!-- begin page-header -->
-<h1 class="page-header">Политравма-детальная таблица</h1>
-<!-- end page-header -->
+    <h1 class="page-header">Политравма-детальная таблица</h1>
+    <div class="row justify-content-center">
+        @foreach($data as $key => $item)
+            <div class="col-md-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Форма {{$item->id}}</h5>
+                        <table class="table table-striped table-bordered align-middle">
+                            <tbody>
+                                <tr>
+                                    <th>id</th>
+                                    <td>{{$item->id}}</td>
+                                </tr>
+                                <tr>
+                                    <th>department</th>
+                                    <td>{{$item->department}}</td>
+                                </tr>
+                                <tr>
+                                    <th>history_disease</th>
+                                    <td>{{$item->history_disease}}</td>
+                                </tr>
+                                <tr>
+                                    <th>full_name</th>
+                                    <td>{{$item->full_name}}</td>
+                                </tr>
+                                <tr>
+                                    <th>hospitalization_date</th>
+                                    <td>{{$item->hospitalization_date}}</td>
+                                </tr>
+                                <tr>
+                                    <th>discharge_date</th>
+                                    <td>{{$item->discharge_date}}</td>
+                                </tr>
+                                <tr>
+                                    <th>hospitalization_channels</th>
+                                    <td>{{$item->hospitalization_channels}}</td>
+                                </tr>
+                                <tr>
+                                    <th>treatment_result</th>
+                                    <td>{{$item->treatment_result}}</td>
+                                </tr>
+                                <tr>
+                                    <th>severity_of_ts</th>
+                                    <td>{{$item->severity_of_ts}}</td>
+                                </tr>
+                                <tr>
+                                    <th>injury_of_iss</th>
+                                    <td>{{$item->injury_of_iss}}</td>
+                                </tr>
+                                <tr>
+                                    <th>arrival_after_injury</th>
+                                    <td>{{$item->arrival_after_injury}}</td>
+                                </tr>
+                                <tr>
+                                    <th>mechanism_of_injury</th>
+                                    <td>{{$item->mechanism_of_injury}}</td>
+                                </tr>
 
-<div class="table-responsive">
-    <table class="table table-striped table-bordered align-middle">
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>department</th>
-                <th>history_disease</th>
-                <th>full_name</th>
-                <th>hospitalization_date</th>
-                <th>discharge_date</th>
-                <th>hospitalization_channels</th>
-                <th>treatment_result</th>
-                <th>severity_of_ts</th>
-                <th>injury_of_iss</th>
-                <th>arrival_after_injury</th>
-                <th>mechanism_of_injury</th>
-                <th>survey_of_surgeon</th>
-                <th>survey_of_neurosurgeon</th>
-                <th>survey_of_traumatologist</th>
-                <th>narrow_specialists</th>
-                <th>r_graphy</th>
-                <th>conducted_ultrasound</th>
-                <th>msct</th>
-                <th>msct_individual_parts</th>
-                <th>neutral_fats</th>
-                <th>analysis_of_hb_ht</th>
-                <th>dynamic_uzs</th>
-                <th>diagnostic_laparoscopy</th>
-                <th>thoracocentesis</th>
-                <th>laparotomy</th>
-                <th>thoracoscopy_thoracotomy</th>
-                <th>osteosynthesis_of_fractures</th>
-                <th>skull_trepanation</th>
-                <th>physician_full_name</th>
-                <th>stat_department_full_name</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($data as $key => $item)
-                <tr>
-                    <td>{{$item->id}}</td>
-                    <td>{{$item->department}}</td>
-                    <td>{{$item->history_disease}} </td>
-                    <td>{{$item->full_name}}</td>
-                    <td>{{$item->hospitalization_date}}</td>
-                    <td>{{$item->discharge_date}}</td>
-                    <td>{{$item->hospitalization_channels}}</td>
-                    <td>{{$item->treatment_result}}</td>
-                    <td>{{$item->severity_of_ts}}</td>
-                    <td>{{$item->injury_of_iss}}</td>
-                    <td>{{$item->arrival_after_injury}}</td>
-                    <td>{{$item->mechanism_of_injury}}</td>
-                    <td>{{$item->survey_of_surgeon}}</td>
-                    <td>{{$item->survey_of_neurosurgeon}}</td>
-                    <td>{{$item->survey_of_traumatologist}}</td>
-                    <td>{{$item->narrow_specialists}}</td>
-                    <td>{{$item->r_graphy}}</td>
-                    <td>{{$item->conducted_ultrasound}}</td>
-                    <td>{{$item->msct}}</td>
-                    <td>{{$item->msct_individual_parts}}</td>
-                    <td>{{$item->neutral_fats}}</td>
-                    <td>{{$item->analysis_of_hb_ht}}</td>
-                    <td>{{$item->dynamic_uzs}}</td>
-                    <td>{{$item->diagnostic_laparoscopy}}</td>
-                    <td>{{$item->thoracocentesis}}</td>
-                    <td>{{$item->laparotomy}}</td>
-                    <td>{{$item->thoracoscopy_thoracotomy}}</td>
-                    <td>{{$item->osteosynthesis_of_fractures}}</td>
-                    <td>{{$item->skull_trepanation}}</td>
-                    <td>{{$item->physician_full_name}}</td>
-                    <td>{{$item->stat_department_full_name}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-
+                                <tr>
+                                    <th>survey_of_surgeon</th>
+                                    <td>{{$item->survey_of_surgeon}}</td>
+                                </tr>
+                                <tr>
+                                    <th>survey_of_neurosurgeon</th>
+                                    <td>{{$item->survey_of_neurosurgeon}}</td>
+                                </tr>
+                                <tr>
+                                    <th>survey_of_traumatologist</th>
+                                    <td>{{$item->survey_of_traumatologist}}</td>
+                                </tr>
+                                <tr>
+                                    <th>narrow_specialists</th>
+                                    <td>{{$item->narrow_specialists}}</td>
+                                </tr>
+                                <tr>
+                                    <th>r_graphy</th>
+                                    <td>{{$item->r_graphy}}</td>
+                                </tr>
+                                <tr>
+                                    <th>conducted_ultrasound</th>
+                                    <td>{{$item->conducted_ultrasound}}</td>
+                                </tr>
+                                <tr>
+                                    <th>msct</th>
+                                    <td>{{$item->msct}}</td>
+                                </tr>
+                                <tr>
+                                    <th>msct_individual_parts</th>
+                                    <td>{{$item->msct_individual_parts}}</td>
+                                </tr>
+                                <tr>
+                                    <th>neutral_fats</th>
+                                    <td>{{$item->neutral_fats}}</td>
+                                </tr>
+                                <tr>
+                                    <th>analysis_of_hb_ht</th>
+                                    <td>{{$item->analysis_of_hb_ht}}</td>
+                                </tr>
+                                <tr>
+                                    <th>dynamic_uzs</th>
+                                    <td>{{$item->dynamic_uzs}}</td>
+                                </tr>
+                                <tr>
+                                    <th>diagnostic_laparoscopy</th>
+                                    <td>{{$item->diagnostic_laparoscopy}}</td>
+                                </tr>
+                                <tr>
+                                    <th>thoracocentesis</th>
+                                    <td>{{$item->thoracocentesis}}</td>
+                                </tr>
+                                <tr>
+                                    <th>laparotomy</th>
+                                    <td>{{$item->laparotomy}}</td>
+                                </tr>
+                                <tr>
+                                    <th>thoracoscopy_thoracotomy</th>
+                                    <td>{{$item->thoracoscopy_thoracotomy}}</td>
+                                </tr>
+                                <tr>
+                                    <th>osteosynthesis_of_fractures</th>
+                                    <td>{{$item->osteosynthesis_of_fractures}}</td>
+                                </tr>
+                                <tr>
+                                    <th>skull_trepanation</th>
+                                    <td>{{$item->skull_trepanation}}</td>
+                                </tr>
+                                <tr>
+                                    <th>physician_full_name</th>
+                                    <td>{{$item->physician_full_name}}</td>
+                                </tr>
+                                <tr>
+                                    <th>stat_department_full_name</th>
+                                    <td>{{$item->stat_department_full_name}}</td>
+                                </tr>          
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
