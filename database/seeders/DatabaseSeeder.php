7f22b9d1d7e80146b0abafe4b65090a205a8407a
ilxomjon123 <abdullayevilxomjon1@gmail.com>
2023-07-04 12:39:14 +0500

    databse seedr changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
+            RoleSeeder::class,
             UserSeeder::class,
             BranchSeeder::class,
         ]);
