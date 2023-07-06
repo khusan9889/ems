baf422f3821519e4aebc2173a590041170e233ae
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-07-06 12:23:19 +0500

    login page translated to ru and put h1 instead of logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,18 @@ module.exports = {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
+        fontSize: {
+            sm: '0.8rem',
+            base: '1rem',
+            xl: '1.25rem',
+            '2xl': '1.563rem',
+            '3xl': '1.953rem',
+            '4xl': '2.441rem',
+            '5xl': '3.052rem',
+        }
     },
-
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms')
+    ]
 };
