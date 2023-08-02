7f6a521d9edf020c35af639cf287be54795d962a
x-code-x-sudo <k.xusan_2003@mail.ru>
2023-08-02 09:57:01 +0500

    nav tabs are added to polytrauma statistics

--- resources/views/dashboard/pages/polytrauma/statistics.blade.php
+++ resources/views/dashboard/pages/polytrauma/statistics.blade.php
@@ -8,6 +8,21 @@
     <x-panel>
         <div class="table-responsive ">
             <table class="table table-striped table-bordered">
+
+
+                <ul class="nav nav-tabs bg-light" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="tab" aria-current="page" href="#">Все</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><16</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">>25</a>
+                    </li>
+                </ul>
+
+
                 <thead>
                 <tr>
                     <th>№</th>
