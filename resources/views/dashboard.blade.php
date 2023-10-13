1b55da056faf3a9abc860fc1b814a652b9664d9c
JumayevIU <ilhomjumayev6022@gmail.com>
2023-10-13 16:51:13 +0500

    add dashbord

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
     <!-- begin row -->
-    <div class="row">
+    <div class="row p-10">
         <!-- begin col-3 -->
         <div class="col-md-4 col-sm-6">
             <div class="widget widget-stats bg-green">
@@ -52,80 +52,90 @@
         <!-- end col-3 -->
     </div>
     <!-- end row -->
-    <div class="row bg-light p-10">
-        <div class="table-responsive col-md-6 col-sm-6 ">
-            <table class="table table-profile">
-                <thead>
-                <tr>
-                    <th>
-                        <h4>Пользовательские данные</h4>
-                    </th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr class="highlight">
-                    <td class="field">ФИО Пользователя</td>
-                    <td>{{$user->name}}</td>
-                </tr>
-                <tr>
-                    <td class="field">Выбрать субъект СЭМП</td>
-                    <td><i class="fa fa-lg m-r-5"></i> {{$user->branch?->name}} </td>
-                </tr>
-                <tr>
-                    <td class="field">Выбрать отделение</td>
-                    <td><a href="#">{{$user->department?->name}}</a></td>
-                </tr>
-                <tr>
-                    <td class="field">Роль</td>
-                    <td>{{$user->role?->name}}</td>
-                </tr>
+    <div class="row p-10">
 
-                <tr class="highlight">
-                    <td class="field">Номер телефона</td>
-                    <td><a href="#">{{$user->phone_number}}</a></td>
-                </tr>
-                <tr class="highlight">
-                    <td class="field">Почта</td>
-                    <td><a href="#">{{$user->email}}</a></td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="table-responsive col-md-6 col-sm-6">
-            <table class="table table-striped align-middle">
-                <thead>
-                <tr>
-                    <th></th>
-                    <th>
-                        <h4>Активности</h4>
-                    </th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <tr>
-                    <th style="width: 60px;">№</th>
-                    <th>Действия</th>
-                    <th>Пользователь</th>
-                    <th>Дата и время</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($data as $key => $item)
-                    <tr>
-                        <td>{{ $item->id }}</td>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->user->name }}, {{ $item->user->email }}</td>
-                        <td>{{ localDatetime($item->created_at) }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-            <div class="d-flex justify-content-center">
-                {{ $data->links() }}
+        <!-- begin col-4 -->
+        <div class="col-md-6">
+            <div class="panel panel-inverse" data-sortable-id="index-6">
+                <div class="panel-heading">
+                    <h4 class="panel-title"> Пользовательские данные</h4>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-profile">
+                        <tbody>
+                        <tr class="highlight">
+                            <td class="field">ФИО Пользователя</td>
+                            <td>{{$user->name}}</td>
+                        </tr>
+                        <tr>
+                            <td class="field">Выбрать субъект СЭМП</td>
+                            <td><i class="fa fa-lg m-r-5"></i> {{$user->branch?->name}} </td>
+                        </tr>
+                        <tr>
+                            <td class="field">Выбрать отделение</td>
+                            <td><a href="#">{{$user->department?->name}}</a></td>
+                        </tr>
+                        <tr>
+                            <td class="field">Роль</td>
+                            <td>{{$user->role?->name}}</td>
+                        </tr>
+
+                        <tr class="highlight">
+                            <td class="field">Номер телефона</td>
+                            <td><a href="#">{{$user->phone_number}}</a></td>
+                        </tr>
+                        <tr class="highlight">
+                            <td class="field">Почта</td>
+                            <td><a href="#">{{$user->email}}</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
 
         </div>
+        <!-- end col-4 -->
+        <!-- begin col-4 -->
+        <div class="col-md-6">
+            <div class="panel panel-inverse" data-sortable-id="index-6">
+                <div class="panel-heading">
+
+                    <h4 class="panel-title"> Активности</h4>
+                </div>
+                <div class="panel-body p-t-0">
+                    <table class="table table-valign-middle m-b-0">
+                        <table class="table table-striped align-middle">
+                            <thead>
+                            <tr>
+                                <th style="width: 60px;">№</th>
+                                <th>Действия</th>
+                                <th>Пользователь</th>
+                                <th>Дата и время</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($data as $key => $item)
+                                <tr>
+                                    <td>{{ $item->id }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->user->name }}, {{ $item->user->email }}</td>
+                                    <td>{{ localDatetime($item->created_at) }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="d-flex justify-content-center">
+                            {{ $data->links() }}
+                        </div>
+
+                    </table>
+                </div>
+            </div>
+
+        </div>
+        <!-- end col-4 -->
     </div>
+
 
 @endsection
