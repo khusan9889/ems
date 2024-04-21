8fdcd9d0247094775ff92bfa745c459775c46da3
Ilhom Jumayev <ilhomjumayev6022@gmail.com>
2024-04-21 22:11:17 +0500

    pdf

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -13,17 +13,12 @@ class PrintController extends Controller
     public function create_pdf(Request $request, $id)
     {
         $acs = ACS::findOrFail($id);
-        // dd($acs);
         $mpdf_uz = new Mpdf();
         $application_file_uz = 'uztest.pdf';
-
         $view_uz = View::make('dashboard.pages.print', compact('acs'));
-
-        return $view_uz;
         $html_content_uz = $view_uz->render();
         $mpdf_uz->WriteHTML($html_content_uz);
         $mpdf_uz->Output('MyPDF.pdf', 'D');
-        // $mpdf_uz->Output(storage_path('app/public/' . $application_file_uz), 'F');
         $uz = 'storage/certificates/' . $application_file_uz;
         return $uz;
     }
