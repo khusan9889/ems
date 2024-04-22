ecb381c614115a69377383f02fc02d6d01ce7dd8
Ilhom Jumayev <ilhomjumayev6022@gmail.com>
2024-04-22 16:57:21 +0500

    push

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -11,14 +11,21 @@ class PrintController extends Controller
 {
     public function create_pdf(Request $request, $id)
     {
-        $acs = ACS::findOrFail($id);
-        $mpdf_uz = new \Mpdf\Mpdf();
-        $application_file_uz = 'uztest.pdf';
-        $view_uz = View::make('dashboard.pages.print', compact('acs'));
-        $html_content_uz = $view_uz->render();
-        $mpdf_uz->WriteHTML($html_content_uz);
-        $mpdf_uz->Output('MyPDF.pdf', 'D');
-        $uz = 'storage/certificates/' . $application_file_uz;
-        return $uz;
+
+        $mpdf = new \Mpdf\Mpdf(); // mPDF obyektini yaratish
+        $mpdf->WriteHTML('<h1>Hello World!</h1>'); // HTML qo'shish
+        $mpdf->Output('doc.pdf', 'D');
+        return $mpdf;
+
+
+//        $acs = ACS::findOrFail($id);
+//        $mpdf_uz = new \Mpdf\Mpdf();
+//        $application_file_uz = 'uztest.pdf';
+//        $view_uz = View::make('dashboard.pages.print', compact('acs'));
+//        $html_content_uz = $view_uz->render();
+//        $mpdf_uz->WriteHTML($html_content_uz);
+//        $mpdf_uz->Output('MyPDF.pdf', 'D');
+//        $uz = 'storage/certificates/' . $application_file_uz;
+//        return $uz;
     }
 }
