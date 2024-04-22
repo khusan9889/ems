ba397165cada782617b7a9c22c34f623a91c4fbf
Ilhom Jumayev <ilhomjumayev6022@gmail.com>
2024-04-22 17:43:08 +0500

    push

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ACS;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Mpdf\Mpdf;
 
 
 class PrintController extends Controller
@@ -12,12 +13,12 @@ class PrintController extends Controller
     public function create_pdf(Request $request, $id)
     {
         $acs = ACS::findOrFail($id);
-        $mpdf_uz = new \Mpdf\Mpdf();
+        $mpdf_uz = new Mpdf( ['tempDir' => '/tmp']);
         $application_file_uz = 'uztest.pdf';
         $view_uz = View::make('dashboard.pages.print', compact('acs'));
         $html_content_uz = $view_uz->render();
         $mpdf_uz->WriteHTML($html_content_uz);
-        $mpdf_uz->Output('MyPDF.pdf', 'D');
+        $mpdf_uz->Output('doc.pdf', 'D');
         $uz = 'storage/certificates/' . $application_file_uz;
         return $uz;
     }
