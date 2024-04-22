968768633ebe738a8cec9a730f3f4d6e48f78c07
Ilhom Jumayev <ilhomjumayev6022@gmail.com>
2024-04-22 20:14:01 +0500

    push

--- app/Http/Controllers/PolytPrintController.php
+++ app/Http/Controllers/PolytPrintController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Polytrauma;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
-use Mpdf\Mpdf;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+
 
 
 class PolytPrintController extends Controller
@@ -13,18 +14,8 @@ class PolytPrintController extends Controller
     public function create_pdf(Request $request, $id)
     {
         $polyt = Polytrauma::findOrFail($id);
-
-        $mpdf_uz = new Mpdf();
-        $application_file_uz = 'uztest.pdf';
-
-        $view_uz = View::make('dashboard.pages.polyt-print', compact('polyt'));
-
-        $html_content_uz = $view_uz->render();
-        $mpdf_uz->WriteHTML($html_content_uz);
-        $mpdf_uz->Output('MyPDF.pdf', 'D');
-        // $mpdf_uz->Output(storage_path('app/public/' . $application_file_uz), 'F');
-        $uz = 'storage/certificates/' . $application_file_uz;
-        return $uz;
+        $pdf = Pdf::loadView('dashboard.pages.polyt-print', ['polyt' => $polyt]);
+        return $pdf->download();
     }
 
 }
